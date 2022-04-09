8963de693427f299543bc2aeb3825ed2551fb7bd
Denzel Braithwaite <braithwaite.d@hotmail.com>
2022-04-09 15:03:00 -0400

    User is able to add/create a task now

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # List of all tasks
-  get '/tasks', to: 'tasks#index'
-  # Show 1 specific task
+  # As a user, I can list all tasks
+  get '/tasks', to: 'tasks#index', as: :index
+
+  # As a user, I can add a new task
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+
+  # As a user, I can find a task
   get '/tasks/:id', to: 'tasks#show', as: :task
+  post '/tasks', to: 'tasks#create'
 end
